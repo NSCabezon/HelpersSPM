ec3f25214e25c1a774cb18dba9309859650b6dd3
NSCabezon <nscabezon@gmail.com>
2020-03-12 17:53:46 +0100

    added extensions to collection

--- Sources/HelpersSPM/Extensions/CollectionExtensions.swift
+++ Sources/HelpersSPM/Extensions/CollectionExtensions.swift
@@ -4,4 +4,14 @@ public extension Collection {
     var isNotEmpty: Bool {
         return !isEmpty
     }
+	
+	func indices(where isIncluded: (Element) throws -> Bool) rethrows -> [Index] {
+		try indices.filter {
+			try isIncluded(self[$0])
+		}
+	}
+}
+
+public extension Collection where Element: Equatable {
+	func indices(of element: Element) -> [Index] { indices.filter { self[$0] == element } }
 }
