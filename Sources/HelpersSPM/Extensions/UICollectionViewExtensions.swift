f32c3ed3c574ba58f614baef89c52249a6cf2dde
NSCabezon <nscabezon@gmail.com>
2020-04-19 11:30:43 +0200

    better naming and fixes in collectionview extension

--- Sources/HelpersSPM/Extensions/UICollectionViewExtensions.swift
+++ Sources/HelpersSPM/Extensions/UICollectionViewExtensions.swift
@@ -2,19 +2,16 @@ import Foundation
 import UIKit
 
 public extension UICollectionView {
-    func register<T: UICollectionViewCell>(_ type: T.Type) {
+    func registerNib<T: UICollectionViewCell>(_ type: T.Type) {
         register(T.nib, forCellWithReuseIdentifier: T.nibName)
     }
     
-    func registerWithBundle<T: UICollectionViewCell>(_ type: T.Type) {
+    func registerNibWithBundle<T: UICollectionViewCell>(_ type: T.Type) {
         register(T.nibWithBundle, forCellWithReuseIdentifier: T.nibName)
     }
     
-    func dequeueReusableCell<T: UICollectionViewCell>(indexPath: IndexPath) -> T {
-        guard let cell = dequeueReusableCell(withReuseIdentifier: T.nibName, for: indexPath) as? T else {
-            fatalError("\(T.nibName) could not be dequeued for \(indexPath) as \(T.self)")
-        }
-        return cell
+	func dequeueReusableCell<T: UICollectionViewCell>(_ cellType: T.Type = T.self, for indexPath: IndexPath) -> T? {
+		return self.dequeueReusableCell(withReuseIdentifier: T.nibName, for: indexPath) as? T
     }
     
     func dequeueReusableSupplementaryView<T: UICollectionViewCell>(kind: String, indexPath: IndexPath) -> T {
