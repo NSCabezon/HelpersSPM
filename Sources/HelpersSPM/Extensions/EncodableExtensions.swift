4ebb2e5cd04c7ba9579f464cf3c2209c8a7bd4c7
NSCabezon <nscabezon@gmail.com>
2020-04-19 14:14:06 +0200

    added jsonString from encodable object

--- Sources/HelpersSPM/Extensions/EncodableExtensions.swift
+++ Sources/HelpersSPM/Extensions/EncodableExtensions.swift
@@ -12,4 +12,15 @@ public extension Encodable {
 		guard let data = try? JSONEncoder().encode(self) else { return nil }
 		return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
 	}
+	
+	var asJSONString: String? {
+		let encodedData = try? JSONEncoder().encode(self)
+		if let data = encodedData,
+			let objString = String(data: data, encoding: String.Encoding.utf8) {
+			return objString
+		} else {
+			assert(false, "something went wrong 😅")
+			return ""
+		}
+	}
 }
