9fee63cb39d159dcb9a8fe3a47d5381768b054bd
Iván Cabezón <nscabezon@gmail.com>
2022-05-26 12:46:40 +0200

    significant decimals

--- Sources/HelpersSPM/Extensions/DoubleExtensions.swift
+++ Sources/HelpersSPM/Extensions/DoubleExtensions.swift
@@ -5,4 +5,12 @@ public extension Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
+
+	func stringWithSignificantDecimals(_ places: Int = 3) -> String {
+		let formatter = NumberFormatter()
+		let number = NSNumber(value: self)
+		formatter.minimumFractionDigits = 0
+		formatter.maximumFractionDigits = places
+		return String(formatter.string(from: number) ?? "")
+	}
 }
--- Sources/HelpersSPM/Extensions/FloatExtensions.swift
+++ Sources/HelpersSPM/Extensions/FloatExtensions.swift
@@ -4,4 +4,12 @@ public extension Float {
     var clean1Decimal: String {
        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.1f", self)
     }
+
+	func stringWithSignificantDecimals(_ places: Int = 3) -> String {
+		let formatter = NumberFormatter()
+		let number = NSNumber(value: self)
+		formatter.minimumFractionDigits = 0
+		formatter.maximumFractionDigits = places
+		return String(formatter.string(from: number) ?? "")
+	}
 }
