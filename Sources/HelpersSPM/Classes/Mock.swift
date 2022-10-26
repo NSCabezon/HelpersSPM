1cdadfbcd558390cf30ec2fc8ad4fd39864fb581
Iván Cabezón <nscabezon@gmail.com>
2022-10-26 17:12:28 +0200

    added throws to mock logic instead of fatalError

--- Sources/HelpersSPM/Classes/Mock.swift
+++ Sources/HelpersSPM/Classes/Mock.swift
@@ -2,40 +2,21 @@ import Foundation
 
 public typealias JSON = [String: Any]
 
-public class Mock {
-	public static func loadJSON<T: Decodable>(_ filename: String, as type: T.Type = T.self, inBundle bundle: Bundle = Bundle.main) -> T {
-		guard let filePath = bundle.path(forResource: filename, ofType: "json") else {
-			fatalError("Couldn’t find \(filename) in main bundle.")
-		}
-        
-        let fileURL = URL(fileURLWithPath: filePath)
-        
-        return loadJSONAtPath(fileURL, as: T.self)
-    }
-    
-    public static func loadJSONAtPath<T: Decodable>(_ filePathURL: URL, as type: T.Type = T.self) -> T {
-        let data: Data
-        do {
-            data = try Data(contentsOf: filePathURL)
-        } catch {
-            fatalError("Couldn’t load \(filePathURL.lastPathComponent) from main bundle:\n\(error)")
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            return try decoder.decode(T.self, from: data)
-        } catch {
-            fatalError("Couldn’t parse \(filePathURL.lastPathComponent) as \(T.self):\n\(error)")
-        }
-    }
+enum MockError: Error {
+    case fileNotFound(fileName: String)
 }
 
-public extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
-    func toJSONString() -> String? {
-        guard let data = try? JSONSerialization.data(withJSONObject: self, options: []),
-            let json = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) else {
-                return nil
-        }
-        return json as String
+public class Mock {
+	public static func loadJSON<T: Decodable>(_ filename: String, as type: T.Type = T.self, inBundle bundle: Bundle = Bundle.main) throws -> T {
+		guard let filePath = bundle.path(forResource: filename, ofType: "json") else {
+            throw MockError.fileNotFound(fileName: filename)
+		}
+        let fileURL = URL(fileURLWithPath: filePath)
+        return try loadJSONAtPath(fileURL, as: T.self)
+    }
+    
+    public static func loadJSONAtPath<T: Decodable>(_ filePathURL: URL, as type: T.Type = T.self) throws -> T {
+        let data = try Data(contentsOf: filePathURL)
+        return try JSONDecoder().decode(T.self, from: data)
     }
 }
--- Sources/HelpersSPM/Extensions/DictionaryExtensions.swift
+++ Sources/HelpersSPM/Extensions/DictionaryExtensions.swift
@@ -1,9 +1,19 @@
 import Foundation
 
-extension Dictionary {
+public extension Dictionary {
     mutating func update(other: Dictionary) {
         for (key, value) in other {
             self.updateValue(value, forKey: key)
         }
+    }
+}
+
+public extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
+    func toJSONString() -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: self, options: []),
+            let json = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) else {
+                return nil
+        }
+        return json as String
     }
 }
